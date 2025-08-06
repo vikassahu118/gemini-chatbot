9fd1d03cbdac67b377d8a898bcfe3fb9641e9953
Vikas Sahu <vikassahu118@gmail.com>
2025-08-06 23:12:29 +0530

    added router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import { UserProvider } from "./context/UserContext.jsx";
 import { ChatProvider } from "./context/ChatContext.jsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.jsx";
 
 export const server = "http://localhost:5000";
 
@@ -11,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
       <ChatProvider>
-        <App />
+        <RouterProvider router={router}/>
       </ChatProvider>
     </UserProvider>
   </React.StrictMode>
---
+++ frontend/src/router.jsx
@@ -0,0 +1,26 @@
+// router.jsx
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Navigate,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Verify from "./pages/Verify";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/verify",
+        element: <Verify />,
+    },
+
+]);
