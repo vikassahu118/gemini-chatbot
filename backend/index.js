6882ac76d99121df897c5ee28fb3b9fb2b391804
Vikas Sahu <vikassahu118@gmail.com>
2025-08-07 16:35:35 +0530

    server

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,25 @@ dotenv.config();
 
 const app = express();
 
-// using middleware
+
 app.use(express.json());
 app.use(cors());
 
-//importing routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+  });
+
+
+
 import userRoutes from "./routes/userRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
 
-//using routes
+
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`server is working on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is working on port ${PORT}`);
   connectDb();
 });
